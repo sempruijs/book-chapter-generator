b2f0b123a7444da17a6051b10b995671445b03f8
sem pruijs <sem@pruijs.net>
2025-02-08 14:05:15 +0100

    Init project

---
+++ app/Main.hs
@@ -0,0 +1,58 @@
+module Main (main) where
+
+import Data.List.Split (splitOn)
+import Data.Maybe (listToMaybe)
+import Debug.Trace
+import System.IO
+
+main :: IO ()
+main = do
+  putStr "Enter file name: "
+  hFlush stdout
+  fileName <- getLine
+  fileHandle <- openFile fileName ReadWriteMode
+  content <- hGetContents fileHandle
+  putStrLn $ "Creating files based on " ++ fileName ++ ": " ++ content
+  applyRequest $ parseRequest fileName content
+  putStrLn "done"
+
+data Request = Request
+  { amount :: Int,
+    fileContent :: Int -> String,
+    fileName :: Int -> String
+  }
+
+applyRequest :: Request -> IO ()
+applyRequest r =
+  sequence_ $
+    map
+      ( \i ->
+          let name = fileName r i
+              content = fileContent r i
+           in writeFile name content
+      )
+      [1 .. (amount r - 1)]
+
+type FileName = String
+
+type FileContent = String
+
+parseRequest :: FileName -> FileContent -> Request
+parseRequest name content =
+  let amount = read $ last $ words $ head $ splitOn "." name
+      fileName i =
+        let withoutExtention = head $ splitOn "." name
+            extention = '.' : (last $ splitOn "." name)
+            withoutNumber = unwords $ init $ words withoutExtention
+         in withoutNumber ++ " " ++ show i ++ extention
+      fileContent i =
+        let line = head $ filter (\s -> listToMaybe s == Just '#') (lines content)
+            beforeHeadingContent = head $ splitOn line content
+            afterHeadingContent = unwords $ tail $ splitOn line content
+            newLine = (unwords $ init $ words line) ++ " " ++ show i
+         in trace (newLine) beforeHeadingContent ++ newLine ++ afterHeadingContent
+   in Request
+        { amount = amount,
+          fileName = fileName,
+          fileContent = fileContent
+        }
